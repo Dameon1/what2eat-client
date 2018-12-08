5ac89eb71368db1a6c7be9db917f473177a5fd91
Dameon Mendoza <dameonmendoza@gmail.com>
2018-12-08 11:41:10 -0700

    checkbox deep testing

--- src/__test__/footer.test.js
+++ src/__test__/footer.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import  Footer  from './Footer';
+import  Footer  from '../components/display/footer';
 
 describe('<Footer />', () => {       
      it('Renders without crashing', () => {
