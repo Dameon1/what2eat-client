4d9cea3d6558584ff5088d1a83d3a742d6a46c0f
Dameon Mendoza <dameonmendoza@gmail.com>
2018-12-08 18:29:09 -0700

    connected Content component tests

--- src/__test__/container.test.js
+++ src/__test__/container.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import { Container } from '../components/containers/container';
+import Container from '../components/containers/container'
 import toJSON from 'enzyme-to-json';
 
 describe('<Container />', () => {       
--- src/__test__/content.test.js
+++ src/__test__/content.test.js
@@ -1,23 +1,25 @@
-
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import  { Content }  from '../components/containers/content';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { testObj, store, configs, dispatch, props } from './testUtils';
 
-describe('<Content />', () => {
-       
-     it('Renders without crashing', () => {
-        const configs = {
-            default: true,
-            label: 'My Label',
-            element: 'myElement',
-            apiRecipes:[12232,1221]
-          }
-        const dispatch = jest.fn();
-
-        const props = {
-          apiRecipes:configs.apiRecipes,
-          dispatch,
-        }
-         shallow(<Content {...props}/>);
+describe('<Content />', () => {    
+  it('Renders without crashing', () => {
+    shallow(<Content {...props}/>);
      });
+
+  it('Renders the item without crashing', () => {
+    const wrapper =  mount(
+            <Provider store={store}>
+              <Router>                
+                <Content dispatch={dispatch} apiRecipes={[testObj]}/>
+              </Router>                
+            </Provider>
+            );
+    expect(wrapper.find('MultipleRecipesDisplay').length).toEqual(1);
+    expect(wrapper.find('h2').text()).toEqual('Apple Tart');
+    expect(wrapper.find('button').text()).toEqual('Get Recipe');
+  });   
 });
